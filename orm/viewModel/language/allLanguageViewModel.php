f82a8094fd35ba33347fc49ecfdac67abb691e24
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2022-11-09 12:59:49 +0100

    Update allLanguageViewModel.php
    Har omstruktureret allLanguageViewModel. så den passer til languageRepository's getAllLangeuageWithId funktionen.

--- orm/viewModel/language/allLanguageViewModel.php
+++ orm/viewModel/language/allLanguageViewModel.php
@@ -3,14 +3,18 @@
         /*********************************/
         /*           properties          */
         /*********************************/
+        public $languageHeadlineId;
+        public $languageHeadlineName;
         public $languageId;
         public $languageName;
 
         /*********************************/
         /*           properties          */
         /*********************************/
-        public function __construct($id, $name)
+        public function __construct($headlineId, $headlineName, $id, $name)
         {
+            $this->languageHeadlineId = $headlineId;
+            $this->languageHeadlineName = $headlineName;
             $this->languageId = $id;
             $this->languageName = $name;
         }
