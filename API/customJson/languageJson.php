ae47fca778b251f926ce065f945f679795bdd24e
Ann-Katrine Nykjær <33825997+Ann-Katrine@users.noreply.github.com>
2022-11-22 16:30:39 +0100

    opret customjson mappe og filer
    startet på at opret mappe og filer til alt min customjson jeg laver. for at splitte min kode mere og så der ikke er kode hvor det ikke er meningen.

---
+++ API/customJson/languageJson.php
@@ -0,0 +1,16 @@
+<?php
+
+    include_once("../orm/viewModel/language/allLanguageViewModel.php");
+
+    class LanguageJson{
+        public function allLanguageWithId($language)
+        {
+            $finish = array();
+
+            $antal = count($language);
+            for ($i = 0; $i < $language; $i++) { 
+                
+            }
+        }
+    }
+?>
